718dbac875e624a2c2adff8cb7d4efeb1481cb10
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-04-14 02:10:01 +0600

    first commit

---
+++ src/pages/Create.js
@@ -0,0 +1,18 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+const Create = () => {
+  return (
+    <div>
+      <Typography 
+      variant="h6" 
+      component="h2" 
+      color="textSecondary" 
+      gutterBottom
+      >
+        Create a new note
+        </Typography> 
+    </div>
+  );
+};
+
+export default Create;
