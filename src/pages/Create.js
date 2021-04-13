a5d69454afe55b2d26c7358314ade42430b5f871
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-04-14 02:31:58 +0600

    4

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,8 +1,12 @@
-import { Typography } from "@material-ui/core";
+import { Button, Container, Typography } from "@material-ui/core";
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import React from "react";
 const Create = () => {
+  const handleClick =()=>{
+    console.log("clicked");
+  }
   return (
-    <div>
+    <Container>
       <Typography 
       variant="h6" 
       component="h2" 
@@ -11,7 +15,25 @@ const Create = () => {
       >
         Create a new note
         </Typography> 
-    </div>
+      <Button
+        onClick={handleClick}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        // startIcon={<SendIcon></SendIcon>}
+        endIcon={<KeyboardArrowRightIcon></KeyboardArrowRightIcon>}
+      >
+        Submit
+      </Button>
+    {/* <br/>
+     <AcUnitOutlinedIcon color="secondary" fontSize="large" ></AcUnitOutlinedIcon>
+     <AcUnitOutlinedIcon color="action" fontSize="small" ></AcUnitOutlinedIcon>
+     <AcUnitOutlinedIcon color="error" fontSize="small" ></AcUnitOutlinedIcon>
+     <AcUnitOutlinedIcon color="disabled" fontSize="small" ></AcUnitOutlinedIcon>
+     <AcUnitOutlinedIcon color="primary" fontSize="small" ></AcUnitOutlinedIcon> */}
+
+
+    </Container>
   );
 };
 
